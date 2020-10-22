9ca0e276cb66b31221e2e5af536e3f4be65697e6
Erik Borela <erikborella@gmail.com>
2020-10-22 20:39:09 -0300

    A* algorithm

---
+++ js/aStar/AStar.js
@@ -0,0 +1,154 @@
+"use strict";
+
+class AStar {
+    /**
+     * 
+     * @param {Number} hSize 
+     * @param {Number} vSize 
+     * @param {JSON} initialNodePosition 
+     * @param {JSON} finalNodePosition 
+     */
+    constructor(hSize, vSize, initialNodePosition, finalNodePosition) {
+        this.board = this._generateBoard(hSize, vSize);
+        this.initialNode = this.board[initialNodePosition.y][initialNodePosition.x];
+        this.finalNode = this.board[finalNodePosition.y][finalNodePosition.x];
+    }
+    
+    /**
+     * 
+     * @param {Number} hSize 
+     * @param {Number} vSize 
+     */
+    _generateBoard(hSize, vSize) {
+        let board = new Array(vSize);
+
+        for (let y = 0; y < vSize; y++) {
+            board[y] = new Array(hSize);
+            for (let x = 0; x < hSize; x++) {
+                board[y][x] = new Node(x, y);
+            }
+        }
+
+        return board;
+    }
+
+    /**
+     * 
+     * @param {Number} x 
+     * @param {Number} y 
+     */
+    _isBoardPositionValid(x, y) {
+        const xBoardSize = this.board[0].length;
+        const yBoardSize = this.board.length;
+
+        return (x >= 0 && x < xBoardSize && y >= 0 && y < yBoardSize);
+    }
+
+    /**
+     * 
+     * @param {Node} node 
+     */
+    openAdjacentNodes(node) {
+        node.isOpen = true;
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+
+                if ((i != 0 || j != 0) && 
+                this._isBoardPositionValid(node.x + j, node.y + i)) {
+
+                    const nextNode = this.board[node.y + i][node.x + j];
+
+                    if (!nextNode.isWall && !nextNode.isOpen) {
+                        nextNode.open(node, this.finalNode);
+                    }
+                }
+            }
+        }
+    }
+
+    findNodeMinorCost() {
+        let minorCostNode = null;
+        
+        const xBoardSize = this.board[0].length;
+        const yBoardSize = this.board.length;
+
+        for (let i = 0; i < yBoardSize; i++) {
+            for (let j = 0; j < xBoardSize; j++) {
+                let node = this.board[i][j];
+
+                if (!node.isWall && !node.isOpen && node.lastNode != null) {
+                    if (minorCostNode == null) {
+                        minorCostNode = node;
+                    } else {
+                        if (node.totalCost < minorCostNode.totalCost) {
+                            minorCostNode = node;
+                        }
+                    }
+                }
+            }
+        }
+
+        return minorCostNode;
+    }
+
+    isFinalAdjacent(node) {
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if ((i != 0 || j != 0) &&
+                this._isBoardPositionValid(node.x + j, node.y + i)) {
+                    const nextNode = this.board[node.y + i][node.x + j];
+
+                    if (nextNode == this.finalNode) {
+                        this.finalNode.lastNode = node;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    _getPathToInitial(node) {
+        let path = [];
+
+        while (node != this.initialNode) {
+            path.push(node);
+            node = node.lastNode;
+        }
+
+        return path;
+    }
+
+    _getPrePaths() {
+        const minorCostNode = this.findNodeMinorCost();
+        let minorsNodes = [];
+
+        const xBoardSize = this.board[0].length;
+        const yBoardSize = this.board.length;
+
+        for (let i = 0; i < yBoardSize; i++) {
+            for (let j = 0; j < xBoardSize; j++) {
+                if (this.board[i][j].totalCost == minorCostNode.totalCost) {
+                    minorsNodes.push(this.board[i][j]);
+                }
+            }
+        }
+
+        let paths = [];
+
+        for (let i = 0; i < minorsNodes.length; i++) {
+            paths.push(this._getPathToInitial(minorsNodes[i]));
+        }
+
+        return paths;
+    }
+
+    getPath() {
+        if (this.finalNode.lastNode == null) {
+            return this._getPrePaths();    
+        } else {
+            return this._getPathToInitial(this.finalNode);
+        }
+    }
+}
--- js/aStar/Node.js
+++ js/aStar/Node.js
@@ -1,6 +1,11 @@
 "use strict";
 
 class Node {
+    /**
+     * 
+     * @param {Number} x 
+     * @param {Number} y 
+     */
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -13,5 +18,51 @@ class Node {
         this.isOpen = false;
 
         this.lastNode = null;
+    }
+
+    _calculateDistances(finalNode) {
+        function euclidianDistance(node1, node2) {
+            const a = Math.pow(Math.abs(node2.x - node1.x), 2);
+            const b = Math.pow(Math.abs(node2.y - node1.y), 2);
+
+            const c = Math.sqrt(a + b);
+
+            const distance = Math.floor(c * 10);
+            return distance;
+        }
+
+        const lastNode = this.lastNode;
+
+        this.gCost = euclidianDistance(this, lastNode);
+        this.gCost += lastNode.gCost;
+
+        this.hCost = euclidianDistance(this, finalNode);
+
+        this.totalCost = this.gCost + this.hCost;
+
+    }
+
+    /**
+     * 
+     * @param {this} lastNode 
+     * @param {this} finalNode 
+     */
+    open(lastNode, finalNode) {
+        if (this.lastNode == null) {
+            this.lastNode = lastNode;
+            this._calculateDistances(finalNode);
+        } else {
+
+            let copyNode = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+            copyNode.lastNode = lastNode;
+            copyNode._calculateDistances(finalNode);
+
+            if (copyNode.totalCost < this.totalCost) {
+                this.gCost = copyNode.gCost;
+                this.hCost = copyNode.hCost;
+                this.totalCost = copyNode.totalCost;
+                this.lastNode = copyNode.lastNode;
+            }
+        }
     }
 }
